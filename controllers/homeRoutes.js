236bf3ba8412ded57b1cf628eb4e34e44f71e57d
brooke <brookejones406@gmail.com>
2021-12-03 21:39:40 -0600

    homeroute added

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,7 +5,9 @@ router.get('/stats', (req, res) => {
     res.sendFile(path.join(__dirname, "../public/stats.html"))
 });
 
-
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+});
 
 router.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/exercise.html"))
